78cbc63ec08b7356ff58bdbb7f54ef784011893d
STB <sillytinybird@gmail.com>
2023-05-17 18:02:00 +0700

    bunch of GUI fixes

--- Assets/Scripts/PlaymodeInterfaceScript.cs
+++ Assets/Scripts/PlaymodeInterfaceScript.cs
@@ -7,12 +7,15 @@ public class PlaymodeInterfaceScript : MonoBehaviour
     [SerializeField] private GameObject _deathScreenGroup;
     [SerializeField] private GameObject _overlayGroup;
     [SerializeField] private GameObject _loadingGroup;
+    [SerializeField] private GameObject _panel;
     [SerializeField] private TextMeshProUGUI _curScore;
     [SerializeField] private TextMeshProUGUI _newScore;
     [SerializeField] private TextMeshProUGUI _deathMessage;
+    [SerializeField] private TextMeshProUGUI _statsText;
     [SerializeField] private DeathMessageScript _scriptWeGetDeathMessageFrom;
     [SerializeField] private FailSFX _scriptForDeathSFX;
     [SerializeField] private IronSourceAdsScript _ads;
+    [SerializeField] private StatsMessage _stats;
     public void SetPause(bool isPaused)
     {
         if (isPaused)
@@ -42,11 +45,13 @@ public class PlaymodeInterfaceScript : MonoBehaviour
             {
                 //do something about it
             }
-            _curScore.SetText(ScoreSystem.Instance.GetScore().ToString("000000"));
+            _curScore.SetText(ScoreSystem.Instance.Score.ToString("000000"));
             _newScore.SetText(FileIO.ReadInt("scoreData.bin").ToString("000000"));//yeah not so clean i guess
             _deathMessage.SetText(_scriptWeGetDeathMessageFrom.GetDeathMessage());
+            _statsText.SetText(_stats.GetStatsString());
             _scriptForDeathSFX.PlayDeathSFX();
             _deathScreenGroup.SetActive(true);
+            _panel.SetActive(true);
             _overlayGroup.SetActive(false);
         }
     }
@@ -61,6 +66,7 @@ public class PlaymodeInterfaceScript : MonoBehaviour
     public void RewardedAdCompleete()
     {
         _deathScreenGroup.SetActive(false);
+        _panel.SetActive(false);
         _overlayGroup.SetActive(true);
         SetPause(false);
     }
